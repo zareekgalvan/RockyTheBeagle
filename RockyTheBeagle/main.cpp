353eedd383dbb6754f6293bb15a74b86a459ecca
Zareek Galván <zareek99@gmail.com>
2015-10-28 14:40:13 -0600

    Initial Commit

---
+++ RockyTheBeagle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RockyTheBeagle
+//
+//  Created by Zareek Galvan on 10/28/15.
+//  Copyright © 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
